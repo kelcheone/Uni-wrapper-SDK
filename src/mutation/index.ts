f716da51915c0fb1ad0e5f3f95462689a8a2cfb3
kelcheone <kelcheone@gmail.com>
2021-10-30 21:59:34 +0300

    feat[wrapping]

--- src/mutation/index.ts
+++ src/mutation/index.ts
@@ -1,1 +1,36 @@
-// TODO
+import {
+  Uni_Query,
+  Uni_Mutation,
+  Input_simpleSwap,
+  SwapOutput,
+  Uni_TradeType,
+} from "./w3";
+
+export function simpleSwap(input: Input_simpleSwap): SwapOutput {
+  const tokenIn = Uni_Query.fetchTokenData({
+    chainId: input.chainId,
+    address: input.tokenInAddress,
+    symbol: null,
+    name: null,
+  });
+
+  const tokenOut = Uni_Query.fetchTokenData({
+    chainId: input.chainId,
+    address: input.tokenOutAddress,
+    symbol: null,
+    name: null,
+  });
+
+  const txResponse = Uni_Mutation.swap({
+    tokenIn: tokenIn,
+    tokenOut: tokenOut,
+    amount: input.tokenInAmount,
+    tradeType: Uni_TradeType.EXACT_INPUT,
+    tradeOptions: input.tradeOptions,
+    txOverrides: null,
+  });
+
+  return {
+    txHash: txResponse.hash,
+  };
+}
--- src/query/index.ts
+++ src/query/index.ts
@@ -1,1 +1,18 @@
-// TODO
+import { Uni_Query, Uni_TokenAmount, Input_fetchTokenTotalSupply } from "./w3";
+
+export function fetchTokenTotalSupply(
+  input: Input_fetchTokenTotalSupply
+): Uni_TokenAmount {
+  const token = Uni_Query.fetchTokenData({
+    chainId: input.chainId,
+    address: input.address,
+    symbol: input.symbol,
+    name: input.name,
+  });
+
+  const amount = Uni_Query.fetchTotalSupply({
+    token: token,
+  });
+
+  return amount;
+}
